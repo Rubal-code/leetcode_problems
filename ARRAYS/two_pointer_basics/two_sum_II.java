cfc97a2753df14b53f3a3bc49ed4557339485f82
Rubal-code <rubal3232@gmail.com>
2026-02-22 20:36:02 +0530

    day 1 of leetcode problems

---
+++ ARRAYS/two_pointer_basics/two_sum_II.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int ans[]=new int[2];
+        int start=0;
+        int end=nums.length-1;
+
+        while(start<end){
+            int sum=nums[start]+nums[end];
+
+            if(sum==target){
+                ans[0]=start+1;
+                ans[1]=end+1;
+                return ans;
+            }
+            else if(sum>target){
+                end--;
+            }
+            else{
+                start++;
+            }
+
+        }
+        return ans;
+    }
+}
