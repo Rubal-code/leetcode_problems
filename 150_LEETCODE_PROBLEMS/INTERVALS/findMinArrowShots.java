2350cbcfecb6d3d099b94fd312e9e1830d6de8c3
Rubal-code <rubal3232@gmail.com>
2026-04-11 19:47:47 +0530

    dY 17

---
+++ 150_LEETCODE_PROBLEMS/INTERVALS/findMinArrowShots.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findMinArrowShots(int[][] points) {
+        if(points==null || points.length==0){
+            return 0;
+        }
+        // 1) sort by endpoint
+        Arrays.sort(points,(a,b)->Integer.compare(a[1],b[1]));
+        int arrows=1;
+        int end=points[0][1];
+        for(int i=1;i<points.length;i++){
+            // If current balloon starts after last arrow position
+            if(points[i][0] > end){
+                arrows++;        // Need new arrow
+                end=points[i][1];// update the new position
+            }
+        }
+        return arrows;
+    }
+}
